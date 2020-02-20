7340c2b7203009f32f853d9f64df0d314148913c
henriquexavie <henrique.xavier2009@gmail.com>
2020-02-20 13:18:15 -0300

    Mapeando JPA e integrando com o banco H2

--- src/main/java/com/controle/controledegastos/resources/CategoriaResource.java
+++ src/main/java/com/controle/controledegastos/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.controle.controledegastos.resources;
 
+import com.controle.controledegastos.domain.Categoria;
+import com.controle.controledegastos.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "rest está funcionando";
+    @Autowired
+    private CategoriaService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id){
+        Categoria obj = service.buscar(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
---
+++ src/main/java/com/controle/controledegastos/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.controle.controledegastos.services;
+
+import com.controle.controledegastos.domain.Categoria;
+import com.controle.controledegastos.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+
+        return obj.orElse(null);
+    }
+
+
+
+}
