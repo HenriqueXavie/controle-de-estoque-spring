eff44f71037a41b8cd657ff0bfcfbd590cec4638
Henrique Xavier <henrique.xavier2009@gmail.com>
2020-06-04 16:03:06 -0300

    Adcionando formas de pagamento

---
+++ src/main/java/com/controle/controledegastos/domain/Pagamento.java
@@ -0,0 +1,68 @@
+package com.controle.controledegastos.domain;
+
+import com.controle.controledegastos.enums.EstadoPagamento;
+import com.sun.javafx.beans.IDProperty;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Pagamento  implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Integer id;
+    private EstadoPagamento estadoPagamento;
+
+    @OneToOne
+    @JoinColumn(name = "pedido_id")
+    @MapsId
+    private Pedido pedido;
+
+    public Pagamento(){}
+
+    public Pagamento(Integer id, EstadoPagamento estadoPagamento, Pedido pedido) {
+        this.id = id;
+        this.estadoPagamento = estadoPagamento;
+        this.pedido = pedido;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public EstadoPagamento getEstadoPagamento() {
+        return estadoPagamento;
+    }
+
+    public void setEstadoPagamento(EstadoPagamento estadoPagamento) {
+        this.estadoPagamento = estadoPagamento;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pagamento pagamento = (Pagamento) o;
+        return Objects.equals(id, pagamento.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
