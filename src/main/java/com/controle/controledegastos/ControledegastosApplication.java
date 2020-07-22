3fc4e3d7c41c6fa065a3ff84f7c67af74d0b74f9
Henrique Xavier <henrique.xavier2009@gmail.com>
2020-07-21 22:35:36 -0300

    itemPedido pk e itemProduto pk

--- src/main/java/com/controle/controledegastos/ControledegastosApplication.java
+++ src/main/java/com/controle/controledegastos/ControledegastosApplication.java
@@ -41,6 +41,9 @@ public class ControledegastosApplication implements CommandLineRunner {
     @Autowired
     private PagamentoRepository pagamentoRepository;
 
+    @Autowired
+    private ItemPedidoRepository itemPedidoRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(ControledegastosApplication.class, args);
     }
@@ -103,5 +106,16 @@ public class ControledegastosApplication implements CommandLineRunner {
         pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
         pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 
+        ItemPedido ip1 = new ItemPedido(ped1, produto1, 0.00, 1, 2000.00);
+        ItemPedido ip2 = new ItemPedido(ped1, produto3, 0.00, 2, 80.00);
+        ItemPedido ip3 = new ItemPedido(ped2, produto2, 100.00, 1, 800.00);
+        ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+        ped2.getItens().addAll(Arrays.asList(ip3));
+
+        produto1.getItens().addAll(Arrays.asList(ip1));
+        produto2.getItens().addAll(Arrays.asList(ip3));
+        produto3.getItens().addAll(Arrays.asList(ip2));
+
+        itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
     }
 }
